2b3ae870af942d85bd7652780398128e2d978972
Delaram Sudo <DelaramSudo@Gmail.com>
2016-09-09 01:19:50 +0430

    Update taylor-bot.lua

--- bot/taylor-bot.lua
+++ bot/taylor-bot.lua
@@ -233,23 +233,9 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {68747297},--Sudo users
+    sudo_users = {232006008,201179826},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Taylor Team v4
-    Taylor Team and Taylor Bot Anti spam / anti link
-    
-    website : 
-    taylortg.ir  ❤️
-    
-    admin : 
-    
-    @negative_officiall ❤️
-    
-    
-    channel : 
-    
-    
-    @taylor_Team ❤️
+    about_text = [[
 ]],
     help_text_realm = [[
 Realm Commands:
